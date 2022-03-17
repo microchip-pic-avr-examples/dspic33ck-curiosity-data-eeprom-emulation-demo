83a9881c96bf3be2230d09887f87e830079e4994
Microchip Technology <microchip@microchip.com>
2022-03-17 12:28:59 +0530

    Added MPLABX Project

---
+++ dspic33c-data-eeprom-emulation-demo/dspic33c-data-eeprom-emulation-demo.X/mcc_generated_files/dee/dee.h
@@ -0,0 +1,105 @@
+/**
+  DEE Generated Driver API Header File
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    dee.h
+
+  @Summary
+    This is the generated header file for the DEE library using Melody Code Configurator
+
+  @Description
+    This header file provides APIs for library DEE.
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 v1.61
+        MPLAB 	          :  MPLABX v5.45
+*/
+
+#ifndef DEE_H
+#define DEE_H
+
+#include <stdint.h>
+
+typedef enum{
+    DEE_NO_ERROR,
+    DEE_EXPIRED_PAGE,
+    DEE_PACK_BEFORE_PAGE_FULL,
+    DEE_PACK_BEFORE_INIT,
+    DEE_PACK_SKIPPED,
+    DEE_ILLEGAL_ADDRESS,
+    DEE_PAGE_CORRUPT,
+    DEE_WRITE_ERROR,
+    DEE_READ_ERROR,
+}DEE_RETURN_STATUS;
+
+
+/**
+  @Summary
+    This function initializes all of the EEPROM banks
+
+  @Description
+    This routine finds an unexpired page to become an active page. It then
+    counts the number of active pages. If no active pages are found, the
+    first unexpired page is initialized for emulation. If one active page is
+    found, it is assumes a reset occurred and the function does nothing. If
+    two active pages are found, it is assumes a reset occurred during a pack.
+    The second page is erased and a pack is called. If three, an error code
+    is returned as the allocated memory is assumed to be corrupted. This
+    function must be called prior to any other operation.
+
+  @Param
+    None.
+
+  @Returns 
+    Returns DEE_RETURN_STATUS
+*/
+DEE_RETURN_STATUS  DEE_Init(void);
+
+/**
+  @Summary
+    This function is used to retrieve data EEPROM information.
+
+  @Description
+    This routine verifies the address is valid. If not, the Illegal Address
+    flag is set and return DEE_ILLEGAL_ADDRESS. It then finds the active page,if an
+    active page can not be found, the Page Corrupt status bit is set and
+    DEE_PAGE_CORRUPT is returned. A reverse search of the active page attempts to find
+    the matching address in the program memory MSB (odd address). If a match
+    is found, the corresponding data EEPROM data (even address) is returned,
+    otherwise DEE_READ_ERROR is returned. This function can be called by the user.
+
+  @Param
+     Data EE address and pointer to hold the read flash data.
+
+  @Returns 
+    Returns DEE_RETURN_STATUS
+*/
+DEE_RETURN_STATUS DEE_Read(uint16_t address, uint16_t *data);
+
+/**
+  @Summary
+    This function is used to write emulated data EEPROM.
+
+  @Description
+    This routine verifies the address is valid. If not, the Illegal Address
+    flag is set and an error code is returned. It then finds the active page.
+    If an active page can not be found, the Page Corrupt status bit is set
+    and an error code is returned. A read is performed, if the data was not
+    changed, the function exits. If the last location is programmed, the Pack
+    Skipped error flag is set (one location should always be available). The
+    data EE information (MSB = address, LSW = data) is programmed and
+    verified. If the verify fails, the Write Error flag is set. If the write
+    went into the last location of the page, pack is called. This function
+    can be called by the user.
+
+  @Param
+    Data EE address and data to be written.
+
+  @Returns 
+    Returns DEE_RETURN_STATUS
+*/
+DEE_RETURN_STATUS DEE_Write(uint16_t address, uint16_t data);
+
+#endif	/* DEE_H */
---
+++ dspic33c-data-eeprom-emulation-demo/dspic33c-data-eeprom-emulation-demo.X/mcc_generated_files/flash/flash_interface.h
@@ -0,0 +1,77 @@
+
+/**
+ * flash Generated Driver Interface Header File
+ * 
+ * @file        flash_interface.h
+ * 
+ * @defgroup    flashdriver Flash Driver
+ * 
+ * @brief       FLASH Driver using dsPIC MCUs
+ *
+ * @version     Driver Version 1.0.0
+ *
+ * @skipline    Device : dsPIC33CK256MP508
+*/
+
+/*
+© [2022] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef FLASH_INTERFACE_H
+#define FLASH_INTERFACE_H
+
+// Section: Included Files
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "flash_types.h"
+
+// Section: Data Type Definitions
+        
+
+/**
+ @ingroup flashdriver
+ @struct FLASH_INTERFACE
+ @brief Structure containing the function pointers of FLASH driver.
+*/
+struct FLASH_INTERFACE
+{   
+    enum FLASH_RETURN_STATUS (*PageErase)(flash_adr_t address, flash_key_t unlockKey); 
+    ///< Pointer to FLASH_PageErase e.g. \ref FLASH_PageErase
+    
+    enum FLASH_RETURN_STATUS (*Write)(flash_adr_t address, flash_data_t* data, flash_key_t unlockKey);
+    ///< Pointer to FLASH_WordWrite e.g. \ref FLASH_WordWrite
+    
+    enum FLASH_RETURN_STATUS (*RowWrite)(flash_adr_t address, flash_data_t* data, flash_key_t unlockKey);
+    ///< Pointer to FLASH_RowWrite e.g. \ref FLASH_RowWrite
+    
+    enum FLASH_RETURN_STATUS (*Read)(flash_adr_t address, size_t count, uint32_t *data);
+    ///< Pointer to FLASH_Read e.g. \ref FLASH_Read
+    
+    uint32_t (*PageAddressGet)(flash_adr_t address);
+    ///< Pointer to FLASH_ErasePageAddressGet e.g. \ref FLASH_ErasePageAddressGet
+    
+    uint16_t (*PageOffsetGet)(flash_adr_t address);
+    ///< Pointer to FLASH_ErasePageOffsetGet e.g. \ref FLASH_ErasePageOffsetGet
+};
+
+#endif //FLASH_INTERFACE_H
+
+
